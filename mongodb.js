e11a9691531fb18b5fef27a83e6c44b5995f5307
Zicheng <zichengshan@gmail.com>
2022-07-12 00:36:55 -0700

    Mongoose

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,11 @@
 // CRUD create read update delete
 
 // Connect to the database
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectId } = require('mongodb')
+
+const id = new ObjectId()
+console.log(id)
+console.log(id.getTimestamp())
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -12,13 +15,14 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         return console.log('Unable to connect to database!')
     }
 
-    console.log('Success')
-
     const db = client.db(databaseName)
-    
-    db.collection('users').insertOne({
-        name: 'Zicheng',
-        age: 25
+
+    db.collection('tasks').deleteOne({
+        _id: new ObjectId('62cbc3ab0fb77c021488d56c')
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
     
 })
