0c92a9a75d346fb4a9d8908bf5f4a94194b55b4e
Zicheng <zichengshan@gmail.com>
2022-07-13 00:32:53 -0700

    Add two routes for fetching tasks info

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose')
 
 const User = require('./models/user')
 const Task = require('./models/task')
+const { response } = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -47,6 +48,27 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+app.get('/tasks', (req, res) =>{
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task){
+            response.status(404).send()
+        }
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
