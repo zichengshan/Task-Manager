a1fa87305f8a17c96d04194261cc8b0fd08d1a2d
Zicheng <zichengshan@gmail.com>
2022-07-14 14:38:56 -0700

    Allow for removal of users and tasks
    setup the endpoint handler
    attempt to delete the task or user by id

--- src/index.js
+++ src/index.js
@@ -69,6 +69,21 @@ app.get('/users', async (req, res) => {
     }
  })
 
+ app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
     try {
@@ -127,6 +142,20 @@ app.patch('/tasks/:id', async (req, res) =>{
     } catch(err){
         res.status(400).send(err)
     }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(err){  
+        res.status(500).send(err)
+     }
 })
 
 app.listen(port, () => {
